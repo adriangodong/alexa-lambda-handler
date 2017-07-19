c855fc4dd9ce629693cfc748a127b6e1f4698118
Adrian Godong <adrian.godong@gmail.com>
2017-07-18 23:40:27 -0700

    Code.

---
+++ AlexaLambdaHandler.Tests/Helpers/BuiltInIntentBuilder.cs
@@ -0,0 +1,64 @@
+﻿using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+
+namespace AlexaLambdaHandler.Tests.Helpers
+{
+    public static class BuiltInIntentBuilder
+    {
+
+        public static SkillRequest AudioPlayer()
+        {
+            return new SkillRequest()
+            {
+                Request = new AudioPlayerRequest()
+            };
+        }
+
+        public static SkillRequest Intent(string intentName)
+        {
+            return new SkillRequest()
+            {
+                Request = new IntentRequest()
+                {
+                    Intent = new Intent()
+                    {
+                        Name = intentName
+                    }
+                }
+            };
+        }
+
+        public static SkillRequest Launch()
+        {
+            return new SkillRequest()
+            {
+                Request = new LaunchRequest()
+            };
+        }
+
+        public static SkillRequest PlaybackController()
+        {
+            return new SkillRequest()
+            {
+                Request = new PlaybackControllerRequest()
+            };
+        }
+
+        public static SkillRequest SessionEnded()
+        {
+            return new SkillRequest()
+            {
+                Request = new SessionEndedRequest()
+            };
+        }
+
+        public static SkillRequest SystemException()
+        {
+            return new SkillRequest()
+            {
+                Request = new SystemExceptionRequest()
+            };
+        }
+
+    }
+}
---
+++ AlexaLambdaHandler.Tests/Helpers/MockHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Amazon.Lambda.Core;
+
+namespace AlexaLambdaHandler.Tests.Helpers
+{
+    internal class MockHandler
+    {
+        private readonly SkillResponse skillResponse = ResponseBuilder.Empty();
+
+        public bool HandlerCalled { get; private set; }
+
+        public Task<SkillResponse> HandlerAsync(SkillRequest request, Request anyRequest, ILambdaContext context)
+        {
+            return Task.FromResult(Handler(request, anyRequest, context));
+        }
+
+        public SkillResponse Handler(SkillRequest request, Request anyRequest, ILambdaContext context)
+        {
+            return DefaultHandler(request, context);
+        }
+
+        public Task<SkillResponse> DefaultHandlerAsync(SkillRequest request, ILambdaContext context)
+        {
+            return Task.FromResult(DefaultHandler(request, context));
+        }
+
+        public SkillResponse DefaultHandler(SkillRequest request, ILambdaContext context)
+        {
+            HandlerCalled = true;
+            return skillResponse;
+        }
+    }
+}
